910e45ab4c41a5628be84c0cd83f8206cf549ada
fgvhjm <surajnie123@gmail.com>
2025-01-12 12:05:46 +0530

    colour

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function info() {
   return {
     apiversion: "1",
     author: "",       // TODO: Your Battlesnake Username
-    color: "#888888", // TODO: Choose color
+    color: "blue", // TODO: Choose color
     head: "default",  // TODO: Choose head
     tail: "default",  // TODO: Choose tail
   };
